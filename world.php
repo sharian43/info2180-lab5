2a7013dc242a295de8c4006a6ea6c9f7b56d129b
sharian43 <shariant98@gmail.com>
2023-11-14 17:27:47 -0500

    Modified php file

--- world.php
+++ world.php
@@ -1,13 +1,18 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
+
+$country = isset($_GET['country']) ? $_GET['country'] : '';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries");
+$stmt->bindParam(':country', $country, PDO::PARAM_STR);
+$stmt->execute();
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 
 ?>
 <ul>
