9f142b8774edbb23590db5c00a4c711d78679f01
sharian43 <shariant98@gmail.com>
2023-11-14 21:18:14 -0500

    Added final touches

--- world.js
+++ world.js
@@ -1,16 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     function lookupCountry() {
-
         var countryInput = document.getElementById('country').value;
+        fetchAndDisplayData('http://localhost/info2180-lab5/world.php?country=' + encodeURIComponent(countryInput));
+    }
 
+    function lookupCities() {
+        var countryInput = document.getElementById('country').value;
+        fetchAndDisplayData('http://localhost/info2180-lab5/world.php?country=' + encodeURIComponent(countryInput) + '&lookup=cities');
+    }
+
+    function fetchAndDisplayData(url) {
         var xhr = new XMLHttpRequest();
-
-        xhr.open('GET', "http://localhost/info2180-lab5/world.php?country=" + encodeURIComponent(countryInput), true);
+        xhr.open('GET', url, true);
 
         xhr.onload = function () {
             if (xhr.status >= 200 && xhr.status < 300) {
-
                 document.getElementById('result').innerHTML = xhr.responseText;
             } else {
                 console.error('Request failed with status', xhr.status);
@@ -25,4 +30,5 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     document.getElementById('lookup').addEventListener('click', lookupCountry);
+    document.getElementById('lookupCities').addEventListener('click', lookupCities);
 });
--- world.php
+++ world.php
@@ -1,45 +1,75 @@
 <?php
+
 $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
+// Allow requests from any origin, you can specify specific origins if needed
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
+header('Content-Type: application/json'); // Adjust content type as needed
 
 // Get the country name from the GET request, default to an empty string if not provided
-$countryName = isset($_GET['country']) ? '%' . $_GET['country'] . '%' : '';
+$countryName = isset($_GET['country']) ? $_GET['country'] : '';
 
-// Use try-catch block to handle potential PDO exceptions
+// Check if the "lookup" parameter is set to "cities"
+$lookupCities = isset($_GET['lookup']) && $_GET['lookup'] === 'cities';
+
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
-    // Use prepared statement to avoid SQL injection
-    $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+    if ($lookupCities) {
+        // If looking up cities, retrieve city information
+        $stmt = $conn->prepare("SELECT cities.name AS city, cities.district, cities.population FROM cities 
+                               JOIN countries ON cities.country_code = countries.code 
+                               WHERE countries.name = :country");
+    } else {
+        // If not looking up cities, retrieve country information
+        $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+    }
+
     $stmt->bindParam(':country', $countryName, PDO::PARAM_STR);
     $stmt->execute();
-
-    // Fetch results only if the query was successful
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    //echo json_encode(['data' => $results]);
 } catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
+    echo json_encode(['error' => $e->getMessage()]);
     exit(); // Terminate script if an error occurs
 }
 ?>
 
-<table border="1">
-    <tr>
-        <th>Country Name</th>
-        <th>Continent</th>
-        <th>Independence Year</th>
-        <th>Head of State</th>
-    </tr>
-    <?php foreach ($results as $row): ?>
+<?php if ($lookupCities): ?>
+    <table border="1">
         <tr>
-            <td><?= $row['name']; ?></td>
-            <td><?= $row['continent']; ?></td>
-            <td><?= $row['independence_year']; ?></td>
-            <td><?= $row['head_of_state']; ?></td>
+            <th>Name</th>
+            <th>District</th>
+            <th>Population</th>
         </tr>
-    <?php endforeach; ?>
-</table>
+        <?php foreach ($results as $row): ?>
+            <tr>
+                <td><?= $row['city']; ?></td>
+                <td><?= $row['district']; ?></td>
+                <td><?= $row['population']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+<?php else: ?>
+    <!-- Display country information as before -->
+    <table border="1">
+        <tr>
+            <th>Country Name</th>
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        </tr>
+        <?php foreach ($results as $row): ?>
+            <tr>
+                <td><?= $row['name']; ?></td>
+                <td><?= $row['continent']; ?></td>
+                <td><?= $row['independence_year']; ?></td>
+                <td><?= $row['head_of_state']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+<?php endif; ?>
